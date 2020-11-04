6ae31c31c4e30b31e406862cf1da56743159714d
Conner Matson <cnr.mtsn@gmail.com>
2020-11-03 20:19:23 -0600

    fixed  build problem

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function home() {
-  return <div></div>;
+export default function Home() {
+  return (
+    <div>
+      <p>
+        This page is soon to be developed. Please explore the other pages in the
+        meantime.
+      </p>
+    </div>
+  );
 }
